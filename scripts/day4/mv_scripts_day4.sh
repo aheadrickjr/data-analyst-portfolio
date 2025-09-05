c019e763815d0e391b269fb3e1eb2f6943c83227
Arval Daniel Headrick Jr. <arval_headrick@yahoo.com>
2025-09-06 07:54:25 +0800

    day4 housekeeping bash shell script

---
+++ scripts/day4/mv_scripts_day4.sh
@@ -0,0 +1,6 @@
+mv run_metadata_sql.sh scripts/sql/
+mv meta_runsqls_day4.sh scripts/day4/
+mv projects/python/py_AutoBuildERD.sh scripts/erd/
+mv projects/python/py_buildTDDCols.sh scripts/dict/
+chmod +x scripts/sql/run_metadata_sql.sh scripts/day4/meta_runsqls_day4.sh scripts/erd/py_AutoBuildERD.sh scripts/dict/py_buildTDDCols.sh
+git add scripts/sql/run_metadata_sql.sh scripts/day4/meta_runsqls_day4.sh scripts/erd/py_AutoBuildERD.sh scripts/dict/py_buildTDDCols.sh
