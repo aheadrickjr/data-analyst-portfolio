484fa5ae08e282f960f5fdc5be711e3e041d3234
Arval Daniel Headrick Jr. <arval_headrick@yahoo.com>
2025-09-08 06:53:40 +0800

    chore(day5): move day5 shell scripts to scripts/labs/day5/

--- scripts/day5_check_zerocnts.sh
+++
@@ -1,6 +0,0 @@
-psql -d "$PGDATABASE" -c "
-SELECT COUNT(*) AS total_rows,
-       COUNT(*) FILTER (WHERE invoice_date IS NULL OR invoice_date='') AS blank_dates,
-       COUNT(*) FILTER (WHERE customer_id IS NULL OR customer_id='')    AS blank_customers,
-       COUNT(*) FILTER (WHERE stock_code  IS NULL OR stock_code='')     AS blank_products
-FROM stage.raw_ecommerce;"
--- scripts/day5_datacheck.sh
+++
@@ -1,5 +0,0 @@
-# how many lines are in the CSV?
-wc -l datasets/ecommerce_sales.csv
-
-# show the first 3 data lines (after the header)
-tail -n +2 datasets/ecommerce_sales.csv | head -n 3 | sed -e 's/,/ [,] /g' -e 's/;/ [;] /g' -e 's/|/ [|] /g' -e 's/\t/ [TAB] /g'
